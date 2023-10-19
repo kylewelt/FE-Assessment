3edf0cd907cfa9cdb4655c58df49ecb4f7bcdf02
Kyle Welton <->
2023-10-19 09:05:43 -0500

    sorts hospitals by rating then shows top 3 sorted

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,19 +5,23 @@ import { HospitalService } from '../hospital.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
   hospitals: Hospital[] = [];
 
-  constructor(private hospitalService: HospitalService) { }
+  constructor(private hospitalService: HospitalService) {}
 
   ngOnInit(): void {
     this.getHospitals();
   }
 
   getHospitals(): void {
-    this.hospitalService.getHospitals()
-      .subscribe(hospitals => this.hospitals = hospitals.slice(0, 3));
+    this.hospitalService.getHospitals().subscribe((hospitals) => {
+      let sortedHospitals = hospitals.sort((a, b) => b.rating - a.rating);
+      let topHospitals = sortedHospitals.slice(0, 3);
+      
+      this.hospitals = topHospitals;
+    });
   }
 }
